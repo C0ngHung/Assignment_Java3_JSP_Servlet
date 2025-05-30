3eb6ced25aa8d50cf3897782d7b00387f544f0f4
C0ngHung <C0ngHungDCH@gmail.com>
2025-05-30 16:10:59 +0700

    Commit02:CongHung:Add_Law_Sport

---
+++ src/main/java/org/example/assignment_java3/user/controller/SportServlet.java
@@ -0,0 +1,21 @@
+package org.example.assignment_java3.user.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/user/sport")
+public class SportServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+        throws IOException, ServletException {
+
+        String page = "/views/pages/user/sport.jsp";
+        req.setAttribute("page", page);
+        req.getRequestDispatcher("/views/layouts/user/layoutUser.jsp").forward(req, resp);
+    }
+}
