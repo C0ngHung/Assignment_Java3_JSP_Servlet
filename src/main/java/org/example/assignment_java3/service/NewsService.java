abdf50371e83d0d64421a62a35e9dd0a1666ff91
C0ngHung <C0ngHungDCH@gmail.com>
2025-06-04 23:24:05 +0700

    Commit16_C0ngHung:UI_Index_50%

--- src/main/java/org/example/assignment_java3/service/NewsService.java
+++ src/main/java/org/example/assignment_java3/service/NewsService.java
@@ -1,4 +1,9 @@
 package org.example.assignment_java3.service;
 
+import org.example.assignment_java3.entity.News;
+
+import java.util.List;
+
 public interface NewsService {
+    List<News> getAllNewsByHome();
 }
